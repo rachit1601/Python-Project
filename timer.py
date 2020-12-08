05a0e668886026e2ad00284371df81bbab61f87a
rachit1601 <70265590+rachit1601@users.noreply.github.com>
2020-12-08 20:20:59 +0530

    Added timer

---
+++ timer.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+t=0
+
+def set_timer():
+  global t
+  t=t+int(entry1.get())
+  return t
+
+def countdown():
+    global t 
+    if t > 0 :
+        L1.config(text=t)
+        t=t-1
+        L1.after(1000,countdown)
+    elif t==0:
+        print("end")
+        L1.config(text="Times up!")
+        
+root = Tk()
+root.geometry("200x150")
+L1 = Label(root , font="ariel")
+L1.grid(row=1,column=2)
+
+times=StringVar()
+entry1=Entry(root,textvariable=times)
+entry1.grid(row=3,column=2)
+
+b1=Button(root,text="SET" , width=20 , command=set_timer)
+b1.grid(row=4,column=2,padx=20)
+
+b2=Button(root,text="START" , width=20 , command=countdown )
+b2.grid(row=6,column=2,padx=20)
+
+root.mainloop()
