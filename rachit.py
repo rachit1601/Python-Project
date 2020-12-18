7129d1e6a55245d7d0295f696e752a9fb14d7af6
rachit1601 <70265590+rachit1601@users.noreply.github.com>
2020-12-18 17:54:20 +0530

    Fixed a lot
    Made functions for most of the things.

--- rachit.py
+++ rachit.py
@@ -61,18 +61,19 @@ def start():
     timerlabel.pack()
     timer_status=0
     timer_start()
+    q_a_main(obj0)
 
-def score_calc(o):
+def score_calc(o , category):
     global score
     global count
     global timer_status
     global timerlabel
     timerlabel.pack()
     timer_status=1
-    coranswer=obj["results"][count-1]['correct_answer'].replace('&#039;', '\'').replace('&quot;', '\"').replace('&lt;','<').replace('&gt;','>')
+    coranswer=category["results"][count-1]['correct_answer'].replace('&#039;', '\'').replace('&quot;', '\"').replace('&lt;','<').replace('&gt;','>')
     if count<15:
         if o == 0:        
-            if answerlist[count-1].get() == obj["results"][count-1]['correct_answer']:
+            if answerlist[count-1].get() == category["results"][count-1]['correct_answer']:
                 score = score+1
                 print(score)
                 tmsg.showinfo("Response", " Correct")
@@ -94,7 +95,7 @@ def score_calc(o):
         timer_start()
     else:
         if o == 0:        
-            if answerlist[count-1].get() == obj["results"][count-1]['correct_answer']:
+            if answerlist[count-1].get() == category["results"][count-1]['correct_answer']:
                 score = score+1
                 print(score)
                 tmsg.showinfo("Response", " Correct")
@@ -113,37 +114,9 @@ def score_calc(o):
 def settings():
     raise_frame(sett_window)
 
-#question list maker
-questions_list=[]
-for i in range (15):
-    qtemp=obj["results"][i]['question']
-    qnew=qtemp.replace('&#039;', '\'').replace('&quot;', '\"')
-    questions_list.append('Q'+str(i+1)+' '+qnew)
-
-#options list 
-new_list=[]
-for i in range(15):
-    opt_list=[]
-    opt_list.append(obj["results"][i]['correct_answer'])
-    opt_list.extend(obj["results"][i]['incorrect_answers'])
-    random.shuffle(opt_list)
-    new_list.append(opt_list)
-
-
-
 themetxt= open('theme.txt','r')
 home = ThemedTk(theme=f'{themetxt.read()}')
-#positionRight = int(home.winfo_screenwidth()/2 - 733/2)
-#positionDown = int(home.winfo_screenheight()/2 - 434/2)
-#home.geometry("733x434+{}+{}".format(positionRight, positionDown))
-home.iconbitmap("logo.ico")
-#home.resizable(False, False)
-timerframe=ttk.Frame(home)
-timerframe.grid(row=1, column=0, sticky='news')
-timerlabel=ttk.Progressbar(timerframe, orient=HORIZONTAL,length=500 ,mode = 'determinate')
-
-timerlabel['value'] = 100
-
+home.title('Quiz')
 
 #frames declaration
 intro= ttk.Frame(home)
@@ -167,17 +140,39 @@ q15=ttk.Frame(home)
 final=ttk.Frame(home)
 #frames declaration end -->
 
+
+#positionRight = int(home.winfo_screenwidth()/2 - 733/2)
+#positionDown = int(home.winfo_screenheight()/2 - 434/2)
+#home.geometry("733x434+{}+{}".format(positionRight, positionDown))
+home.iconbitmap("logo.ico")
+#home.resizable(False, False)
+
+#<--timer frame
+timerframe=ttk.Frame(home)
+timerframe.grid(row=1, column=0, sticky='news')
+#timer frame End-->
+
+#<--Timer label
+timerlabel=ttk.Progressbar(timerframe, orient=HORIZONTAL,length=500 ,mode = 'determinate')
+timerlabel['value'] = 100
+#timer label -->
+
+
+
 #themes names button
-ttk.Label(sett_window,text='Select your theme').pack(pady=10)
-ttk.Button(sett_window,text='Equilux',command=lambda:themeset('equilux')).pack(pady=10)
-ttk.Button(sett_window,text='Yaru',command=lambda:themeset('yaru')).pack(pady=10)
-ttk.Button(sett_window,text='Radiance',command=lambda:themeset('radiance')).pack(pady=10)
-ttk.Button(sett_window,text='Black',command=lambda:themeset('black')).pack(pady=10)
-ttk.Button(sett_window,text='Default',command=lambda:themeset('default')).pack(pady=10)
-ttk.Button(sett_window,text='Breeze',command=lambda:themeset('breeze')).pack(pady=10)
-ttk.Button(sett_window,text='ITFT1',command=lambda:themeset('itft1')).pack(pady=10)
-ttk.Button(sett_window,text='Return to Mainmenu',command=lambda:raise_frame(intro)).pack(pady=10)
-#themes names button end -->
+def themes_buttons():
+    
+    ttk.Label(sett_window,text='Select your theme').pack(pady=10)
+    ttk.Button(sett_window,text='Equilux',command=lambda:themeset('equilux')).pack(pady=10)
+    ttk.Button(sett_window,text='Yaru',command=lambda:themeset('yaru')).pack(pady=10)
+    ttk.Button(sett_window,text='Radiance',command=lambda:themeset('radiance')).pack(pady=10)
+    ttk.Button(sett_window,text='Black',command=lambda:themeset('black')).pack(pady=10)
+    ttk.Button(sett_window,text='Default',command=lambda:themeset('default')).pack(pady=10)
+    ttk.Button(sett_window,text='Breeze',command=lambda:themeset('breeze')).pack(pady=10)
+    ttk.Button(sett_window,text='ITFT1',command=lambda:themeset('itft1')).pack(pady=10)
+    ttk.Button(sett_window,text='Return to Mainmenu',command=lambda:raise_frame(intro)).pack(pady=10)
+    #themes names button end -->
+themes_buttons()
 
 #frame assignment
 for frame in (intro,cate,sett_window, q1, q2, q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,final):
@@ -186,57 +181,81 @@ for frame in (intro,cate,sett_window, q1, q2, q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q
 #home page
 photo=PhotoImage(file="quiz_trans.png")
 pic=ttk.Label(intro, image=photo, borderwidth=0).pack()
+
 start_img=PhotoImage(file='start.png')
 ttk.Button(intro, image=start_img ,command=start).pack(side='bottom')
+
 settings_img=PhotoImage(file='settings.png')
 ttk.Button(intro, image=settings_img,command=settings).pack(side='bottom')
 
 
+answerlist=[]
+def q_a_main(category):
+    global answerlist
+    #question list maker
+    questions_list=[]
+    for i in range (15):
+        qtemp=category["results"][i]['question']
+        qnew=qtemp.replace('&#039;', '\'').replace('&quot;', '\"')
+        questions_list.append('Q'+str(i+1)+' '+qnew)
 
+    #options list 
+    new_list=[]
+    for i in range(15):
+        opt_list=[]
+        opt_list.append(category["results"][i]['correct_answer'])
+        opt_list.extend(category["results"][i]['incorrect_answers'])
+        random.shuffle(opt_list)
+        new_list.append(opt_list)
 
-#question labels in diff. frames
-j=0
-for i in (q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15):
-    question=ttk.Label(i, text=questions_list[j], font='lucida 20 bold'  , wraplength=400).pack(pady=20)
-    j=j+1
-
-
- 
-
-
-#variables of answers
-ans1 = StringVar()
-ans2 = StringVar()
-ans3 = StringVar()
-ans4 = StringVar()
-ans5 = StringVar()
-ans6 = StringVar()
-ans7 = StringVar()
-ans8 = StringVar()
-ans9 = StringVar()
-ans10 = StringVar()
-ans11 = StringVar()
-ans12 = StringVar()
-ans13 = StringVar()
-ans14 = StringVar()
-ans15 = StringVar()
-#answer variable list
-answerlist=[ans1,ans2,ans3,ans4,ans5,ans6,ans7,ans8,ans9,ans10,ans11,ans12,ans13,ans14,ans15]
+    #question labels in diff. frames
+    j=0
+    for i in (q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15):
+        question=ttk.Label(i, text=questions_list[j], font='lucida 20 bold'  , wraplength=400).pack(pady=20)
+        j=j+1
 
 
 
-#option radio buttons
-noq=0
-for q in (q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15):
-    options= ttk.Radiobutton(q, text= new_list[noq][0].replace('&#039;', '\'').replace('&quot;', '\"').replace('&lt;','<').replace('&gt;','>'),variable=answerlist[noq],value=new_list[noq][0]).pack(pady=10)
-    options= ttk.Radiobutton(q, text= new_list[noq][1].replace('&#039;', '\'').replace('&quot;', '\"').replace('&lt;','<').replace('&gt;','>'),variable=answerlist[noq],value=new_list[noq][1]).pack(pady=10)
-    options= ttk.Radiobutton(q, text= new_list[noq][2].replace('&#039;', '\'').replace('&quot;', '\"').replace('&lt;','<').replace('&gt;','>'),variable=answerlist[noq],value=new_list[noq][2]).pack(pady=10)
-    options= ttk.Radiobutton(q, text= new_list[noq][3].replace('&#039;', '\'').replace('&quot;', '\"').replace('&lt;','<').replace('&gt;','>'),variable=answerlist[noq],value=new_list[noq][3]).pack(pady=10)
-    noq=noq+1
     
-#Submit Buttom
-for j in (q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15):
-    ttk.Button(j, text="Submit" ,command=lambda:score_calc(0)).pack(side='bottom', pady=10)
+
+
+    #variables of answers
+    ans1 = StringVar()
+    ans2 = StringVar()
+    ans3 = StringVar()
+    ans4 = StringVar()
+    ans5 = StringVar()
+    ans6 = StringVar()
+    ans7 = StringVar()
+    ans8 = StringVar()
+    ans9 = StringVar()
+    ans10 = StringVar()
+    ans11 = StringVar()
+    ans12 = StringVar()
+    ans13 = StringVar()
+    ans14 = StringVar()
+    ans15 = StringVar()
+    #answer variable list
+    answerlist=[ans1,ans2,ans3,ans4,ans5,ans6,ans7,ans8,ans9,ans10,ans11,ans12,ans13,ans14,ans15]
+
+
+
+    #option radio buttons
+    noq=0
+    for q in (q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15):
+        options= ttk.Radiobutton(q, text= new_list[noq][0].replace('&#039;', '\'').replace('&quot;', '\"').replace('&lt;','<').replace('&gt;','>'),variable=answerlist[noq],value=new_list[noq][0]).pack(pady=10)
+        options= ttk.Radiobutton(q, text= new_list[noq][1].replace('&#039;', '\'').replace('&quot;', '\"').replace('&lt;','<').replace('&gt;','>'),variable=answerlist[noq],value=new_list[noq][1]).pack(pady=10)
+        options= ttk.Radiobutton(q, text= new_list[noq][2].replace('&#039;', '\'').replace('&quot;', '\"').replace('&lt;','<').replace('&gt;','>'),variable=answerlist[noq],value=new_list[noq][2]).pack(pady=10)
+        options= ttk.Radiobutton(q, text= new_list[noq][3].replace('&#039;', '\'').replace('&quot;', '\"').replace('&lt;','<').replace('&gt;','>'),variable=answerlist[noq],value=new_list[noq][3]).pack(pady=10)
+        noq=noq+1
+    
+    
+    #Submit Buttom
+    for j in (q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15):
+        ttk.Button(j, text="Submit" ,command=lambda:score_calc(0 , category)).pack(side='bottom', pady=10)
+
+            
+
 
     
     
